51aa48140b586be6460e78fff34845894fdbced9
thisisbipin <bipinmjadav087@gmail.com>
2022-01-07 23:55:40 +0530

    basic create-react-app finalized

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 
@@ -7,7 +6,6 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
