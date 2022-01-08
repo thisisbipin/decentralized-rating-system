1f9c6fec78b1953b4626006d70eaba3b6fd6133c
Harsh <patelharsh0603@gmail.com>
2022-01-08 18:09:05 +0530

    Modified Contract : Added user auth using public address, added buy product

--- contracts/RatingContract.sol
+++ contracts/RatingContract.sol
@@ -1,26 +1,31 @@
 pragma solidity ^0.8.9;
 
 contract RatingContract {
+
+    mapping(uint => uint) productPrice; // number of wei for each pId
     
     // For storing the ratings and reviews for each product
     struct ProductReview {
         uint points;
         uint count;
+        mapping(address => bool) validateUser;
         Rating[] reviewList;
     }
-    
+
     // maps the pId (product Id) to the ProductReview object corresponding to that product
-    // products are uniquely identified by pId (product ID)
+    // products are uniquely identified by pId (product ID)    
     mapping(uint => ProductReview) productReviewMap;
     
     // For storing the user reviews for each product
+        //added time, removed cid
     struct Rating {
-        uint id;
-        string cId;
+        string name;
+        uint time;
         uint8 star;
         string review;
     }
     
+    //------------------------ Ratings section ---------------------------------
     // Returns the total points the product has received from users
     function getPoints(uint pId) public view returns (uint) {
         return productReviewMap[pId].points;
@@ -37,11 +42,30 @@ contract RatingContract {
     }
     
     // Function for users to give rating for a product
-    function createRating(uint pId, string memory _cId, uint8 _star, string memory _review) public {
+    function createRating(uint pId, string memory name, uint8 _star, string memory _review) public {
+        require(productReviewMap[pId].validateUser[address(msg.sender)]==true,"Your not allowed to rate");
+        productReviewMap[pId].validateUser[msg.sender]=false;
         productReviewMap[pId].count+=1;
         productReviewMap[pId].points+=_star;
-        productReviewMap[pId].reviewList.push(Rating(productReviewMap[pId].count, _cId, _star, _review));
-    
+        productReviewMap[pId].reviewList.push(Rating(name,block.timestamp, _star, _review));       
     }
+
+    //------------------------------------------------------------------------
+
+
+    //--------------------------- Buy Product section --------------------------------
+    // Function to buy a product and initiate authentication of buyers
+    function buyProduct(uint pId) public payable {
+        require(productPrice[pId]==msg.value,"Please pay the correct price");
+        productReviewMap[pId].validateUser[address(msg.sender)] = true;
+        
+    }
+
+    //TODO : implement better owner authentication
+    function addOrEditPrice(uint pId,uint priceWei) public {
+        productPrice[pId] = priceWei;
+    }
+
+    //--------------------------------------------------------------------------
     
 }
