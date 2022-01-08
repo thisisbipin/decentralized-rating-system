ef2cc851942453c0e18c8299f2f1c104efa977df
thisisbipin <bipinmjadav087@gmail.com>
2022-01-08 12:43:15 +0530

    completed footer and added card images

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import Products from "./components/Products";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   let [productsList, setProductsList] = useState([
     {
       PID: 1,
       Name: "Laptop",
-      ImageURL: "#",
+      ImageURL:
+        "https://i.picsum.photos/id/188/300/300.jpg?hmac=O1Fgsa5q091m_1eNHYtavDoVNb6yIklGq1A67yv5_vk",
       Price: 20,
       Details: "Fetching",
     },
     {
       PID: 2,
       Name: "Phone",
-      ImageURL: "#",
+      ImageURL:
+        "https://i.picsum.photos/id/339/300/300.jpg?hmac=OGGRAko85waj1sgpjCL-gLOfoqFVF4167VXJk3sOx4E",
       Price: 20,
       Details: "Fetching",
     },
@@ -24,7 +27,23 @@ function App() {
   return (
     <>
       <Header />
-      <Products productsList={productsList} />
+      <Router>
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={() => <Products productsList={productsList} />}
+          />
+          <Route
+            path="/product/:id"
+            exact
+            render={(props) => {
+              //product page to be rendered
+              return <></>;
+            }}
+          />
+        </Switch>
+      </Router>
       <Footer />
     </>
   );
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./css/products.css";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 class Products extends React.Component {
   state = {
@@ -22,26 +23,25 @@ class Products extends React.Component {
           </h1>
           <div className="products__products-container">
             {this.state.products.map((eachproduct) => (
-              <div
-                className="products__products-container__card card"
-                key={eachproduct.PID}
-              >
-                <img
-                  src={eachproduct.ImageURL}
-                  className="card-img-top"
-                  alt={eachproduct.Name}
-                />
-                <div className="products__products-container__card__card-body card-body">
-                  <h5 className="products__products-container__card__card-title card-title">
-                    {eachproduct.Name}
-                  </h5>
-                  <p>Rating: 5 ⭐ </p>
-                  <p className="products__products-container__card__card-text card-text">
-                    {eachproduct.Details}
-                  </p>
-                  <a className="btn btn-primary">View Details</a>
+              <Link key={eachproduct.PID} to={"/product/" + eachproduct.PID}>
+                <div className="products__products-container__card card">
+                  <img
+                    src={eachproduct.ImageURL}
+                    className="card-img-top"
+                    alt={eachproduct.Name}
+                  />
+                  <div className="products__products-container__card__card-body card-body">
+                    <h5 className="products__products-container__card__card-title card-title">
+                      {eachproduct.Name}
+                    </h5>
+                    <p>Rating: 5 ⭐ </p>
+                    <p className="products__products-container__card__card-text card-text">
+                      {eachproduct.Details}
+                    </p>
+                    <button className="btn btn-primary">View Details</button>
+                  </div>
                 </div>
-              </div>
+              </Link>
             ))}
           </div>
         </section>
