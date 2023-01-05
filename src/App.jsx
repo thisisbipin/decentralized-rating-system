7bb3a37613f337bdbbfea7787f5c02623886092c
thisisbipin <bipinmjadav087@gmail.com>
2023-01-05 15:44:01 +0530

    new build

--- src/App.jsx
+++ src/App.jsx
@@ -11,46 +11,54 @@ import { db } from "./components/firebase/Fire";
 import fire from "./components/firebase/Fire";
 
 function App() {
-  let [productsList, setProductsList] = useState([]);
-  useEffect(() => {
-    db.collection("productData")
-      .get()
-      .then((querySnapshot) => {
-        let products = [];
-        querySnapshot.forEach((element) => {
-          var data = element.data();
-          // console.log(data);
-          products.push(data);
-        });
-        products = products.sort((a, b) => {
-          return a.PID < b.PID ? -1 : a.PID > b.PID ? 1 : 0;
-        });
-        //console.log(products);
-        setProductsList(products);
-      });
-  }, []);
-  return (
-    <>
-      <Router>
-        <Header />
-        <Switch>
-          <Route
-            path="/"
-            exact
-            render={() => <Products productsList={productsList} />}
-          />
-          <Route
-            path="/product/:id"
-            exact
-            render={(props) => (
-              <ProductDetail {...props} productsList={productsList} />
-            )}
-          />
-        </Switch>
-      </Router>
-      <Footer />
-    </>
-  );
+    let [productsList, setProductsList] = useState([]);
+    useEffect(() => {
+        db.collection("productData")
+            .get()
+            .then((querySnapshot) => {
+                let products = [];
+                querySnapshot.forEach((element) => {
+                    var data = element.data();
+                    // console.log(data);
+                    products.push(data);
+                });
+                products = products.sort((a, b) => {
+                    return a.PID < b.PID ? -1 : a.PID > b.PID ? 1 : 0;
+                });
+                //console.log(products);
+                setProductsList(products);
+            });
+    }, []);
+    return (
+        <>
+            <Router>
+                <Header />
+                <Switch>
+                    <Route
+                        path="/"
+                        exact
+                        render={() => <Products productsList={productsList} />}
+                    />
+                    <Route
+                        path="/decentralized-rating-system/"
+                        exact
+                        render={() => <Products productsList={productsList} />}
+                    />
+                    <Route
+                        path="/product/:id"
+                        exact
+                        render={(props) => (
+                            <ProductDetail
+                                {...props}
+                                productsList={productsList}
+                            />
+                        )}
+                    />
+                </Switch>
+            </Router>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
